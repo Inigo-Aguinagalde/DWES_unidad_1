ea571be2950f781ee084f370f98423dd7905a473
unknown <2daw3@HZ114486.H012108.NET>
2022-09-20 12:14:38 +0200

    Ejercicio 1 hecho

---
+++ actividad5/ejercicio1/ejercicio1Login.php
@@ -0,0 +1,20 @@
+<?php 
+/*
+ * Crear un formulario de login y visulizar la información en pantalla
+    Los campos del formulario serán usuario y contrasena
+    El formulario enviará la información por POST.
+    Al principio hacer la prueba con GET para ver cómo se envían los parámetros.
+    Página de login ejercicio01Login.html
+    PHP del servidor ejercicio 01 Login.php
+
+ */
+?>
+<html>
+<body>
+
+Welcome <?php echo $_POST["name"]; ?><br>
+Your password is: <?php echo $_POST["Password"]; ?>
+
+</body>
+</html> 
+

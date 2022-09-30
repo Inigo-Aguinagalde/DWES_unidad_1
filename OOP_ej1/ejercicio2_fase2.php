d1b84f9a070ec954332c8e62f30b439f41065955
unknown <2daw3@HZ114486.H012108.NET>
2022-09-30 13:37:37 +0200

    terminado

--- OOP_ej1/ejercicio2_fase2.php
+++ OOP_ej1/ejercicio2_fase2.php
@@ -19,47 +19,52 @@ if ($_POST['sobremesa']=="si") {
 }
 
 $jsonData = file_get_contents('Ordenadores.json',true);
-var_dump($jsonData);
-$jsonDecode = json_decode($jsonData,true);
-echo "////////////////////////////////////////";
-var_dump($jsonDecode);
 
-echo "JSON DECODE //////////////////////////// " . $jsonDecode[['Identificador']];
+$jsonDecode = json_decode($jsonData,true);
+
+
+
+
+$tieneDatos= empty($jsonDecode);
+
+
+
+
+
 
 $ordenador = new clOrdenador(); 
 
 $ordenador->setCodHZ($hz);
 $ordenador->setSO($so);
 $ordenador->setEsSobremesa($sobremesa);
-$existe = null;
-
-
+$existe;
 
 
 //Comprobar que el ordenador nuevo existe en el array
 for ($i = 0; $i < count($jsonDecode); $i++) {
-    if($jsonDecode[$i] == $ordenador->getCodHZ()){
+
+    if($jsonDecode[$i]['Identificador'] == $ordenador->getCodHZ()){
         echo "No se puede guardar un ordenador previamente guardado";
         $existe=true;
         break;
     }else{
         $existe=false;
-        echo "El ordenador se ha guardado con exito";
+      
     }
    
 }
 
-print_r($ordenadores);
 
 
 
 if ($existe!=true) {
     
-    $myJson = array([
+    echo "El ordenador se ha guardado con exito";
+    $myJson = array(
         "Sistema operativo" => $ordenador->getSO(),
         "Identificador" => $ordenador->getCodHZ(),
         "Sobremesa" =>$sobremesa
-    ]);
+    );
     
     $jsonDecode[] = $myJson;
 }
@@ -70,7 +75,6 @@ if ($existe!=true) {
 
 $myJson = json_encode($jsonDecode);
 
-var_dump($myJson);
 
 
 
